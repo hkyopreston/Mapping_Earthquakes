926d80f6d1a9c93d2eb7eb7f31c8b3637364a297
Harrison Preston <harrisonpreston@Harrisons-MacBook-Pro.local>
2020-08-08 13:00:40 -0700

    mapping multiple points

---
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,42 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+    center: [
+      40.7, -94.5
+    ],
+    zoom: 4
+});
+
+// Get data from cities.js
+let cityData = cities;
+
+cityData.forEach(function(city) {
+  console.log(city)
+  // L.marker(city.location)
+  L.circleMarker(city.location, {
+    radius: city.population/100000,
+    color: 'orange',
+    fillColor: 'orange',
+    fillOpacity: 0.2
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
+
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecomm
+
+// mapbox://styles/mapbox/outdoors-v11
+
+// We create the tile layer that will be the background of our map.
+// 'https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
