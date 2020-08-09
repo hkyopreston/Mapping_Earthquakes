dec2c5cac5f0df625a83a2af2064fce4cce9a805
Harrison Preston <harrisonpreston@harrisons-mbp.lan>
2020-08-09 10:51:31 -0700

    Adding Polygon Files

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,41 +1,7 @@
-// // Add console.log to check to see if our code is working.
-// console.log("working");
+// Add console.log to check to see if our code is working.
+console.log("working");
 
-// // Create the map object with center and zoom level.
-// let map = L.map('mapid').setView([30, 30], 2);
-
-// // Add GeoJSON data.
-// let sanFranAirport =
-// {"type":"FeatureCollection","features":[{
-//     "type":"Feature",
-//     "properties":{
-//         "id":"3469",
-//         "name":"San Francisco International Airport",
-//         "city":"San Francisco",
-//         "country":"United States",
-//         "faa":"SFO",
-//         "icao":"KSFO",
-//         "alt":"13",
-//         "tz-offset":"-8",
-//         "dst":"A",
-//         "tz":"America/Los_Angeles"},
-//         "geometry":{
-//             "type":"Point",
-//             "coordinates":[-122.375,37.61899948120117]}}
-// ]};
-
-// // Grabbing our GeoJSON data.
-// L.geoJson(sanFranAirport, {
-//   // We turn each feature into a marker on the map.
-//   pointToLayer: function(feature, latlng) {
-//     console.log(feature);
-//     return L.marker(latlng)
-//     .bindPopup("<h2>" + feature.properties.city + "</h2>");
-//   }
-// }).addTo(map);
-
-
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
@@ -50,44 +16,38 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
+  Light: light,
   Dark: dark
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-	center: [30, 30],
+	center: [44.0, -80.0],
 	zoom: 2,
-	layers: [streets]
-})
+	layers: [light]
+});
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// // Then we add our 'graymap' tile layer to the map.
-// streets.addTo(map);
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/hkyopreston/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/hkyopreston/Mapping_Earthquakes/master/majorAirports.json";
-
-
-// // When we use the onEachFeature callback function we can add a popup marker for each feature and 
-// // add data from the properties of the JavaScript object
-// L.geoJson(sanFranAirport, {
-//   onEachFeature: function(feature, layer) {
-//     console.log(layer);
-//     layer.bindPopup("<h2>" + "Airport Code: " + feature.properties.faa + "</h2> <hr> <h3>Airport name: " + feature.properties.name + "</h3>");
-//    }
-// }).addTo(map);
-
-d3.json(airportData).then(function(data) {
+// Create a style for the lines
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
   console.log(data);
-  // GeoJSON layer with data
-  L.geoJSON(data,
-  {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data, {
+    style: myStyle,
     onEachFeature: function(feature, layer) {
-      console.log(layer);
-      layer.bindPopup("<h2>" + "Airport Code: " + feature.properties.faa + "</h2> <hr> <h3>Airport name: " + feature.properties.name + "</h3>");
+      layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination:"
+      + feature.properties.dst + "</h3>");
     }
-  }).addTo(map);
+  })
+  .addTo(map);
 });
